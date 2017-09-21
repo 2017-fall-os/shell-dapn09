c45a205dc3e04213c57724f0cec00ff4b8e0dee8
dapn09 <31325026+dapn09@users.noreply.github.com>
2017-09-21 14:31:35 -0600

    Update launcher.c

--- shell/launcher.c
+++ shell/launcher.c
@@ -7,20 +7,26 @@
 
 int launcher(int argc, char *argv[], char *envp[]){
   int rc = fork();
+  int execRet = 0;
   if (rc < 0) { // fork failed, exit
-    fprintf(stderr, "fork failed\n");
+    write(1, "fork failed\n", 13);
     exit(1);
     } else 
     if (rc == 0) { // child (new process)
-      char *myargs[3];
-      myargs[0] = strdup("wc"); // program: "wc" (word count)
-      myargs[1] = strdup("p3.c"); // argument: file to count
-      myargs[2] = NULL; // marks end of array
-      execve(argv[0], argv, envp); // TODO: must change to execve myargs[0]
+      //char *myargs[3];
+      //myargs[0] = strdup("wc"); // program: "wc" (word count)
+      //myargs[1] = strdup("p3.c"); // argument: file to count
+      //myargs[2] = NULL; // marks end of array
+      execRet = execve(argv[0], argv, envp); // TODO: must change to execve myargs[0]
       write(1, "this shouldn’t print out\n", 25);
     } else { // parent goes down this path (main)
-    int wc = wait(NULL);
-    write(1, "hello, I am parent\n", 19);
-    }
+         int wc = wait(NULL);
+         if(execRet == 0){
+            write(1, "hello, I am parent\n", 19);
+         }else{
+            write(1, "program exited abnormaly\n", 27);
+            return 0;
+         }
+      }
     return 0;
 }
