ac9323c481a34c3e0a264ccb5f140c1a68e00e1c
dapn09 <31325026+dapn09@users.noreply.github.com>
2017-09-21 17:47:02 -0600

    Update myTok.c

--- shell/myTok.c
+++ shell/myTok.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include "myTok.h"
+
+
+int launcher(int argc, char *argv[], char *envp[]){
+  int rc = fork();
+  int execRet = 0;
+  if (rc < 0) { // fork failed, exit
+    write(1, "fork failed\n", 13);
+    exit(1);
+    } else 
+    if (rc == 0) { // child (new process)
+      //char *myargs[3];
+      //myargs[0] = strdup("wc"); // program: "wc" (word count)
+      //myargs[1] = strdup("p3.c"); // argument: file to count
+      //myargs[2] = NULL; // marks end of array
+      execRet = execve(argv[0], argv, envp); // TODO: must change to execve myargs[0]
+      write(1, "Command not found\n\n", 21);
+      exit(1);
+    } 
+    else { // parent goes down this path (main)
+      int wc = wait(NULL);
+    }
+    return 0;
+}
+//this method gets a reference to the envp vector and returns a vector made from the PATH entries.
+char ** getPath(char** envp){
+    
+}
 
 //helper method used to print the 2D array
 void print2DArray(char** array){
