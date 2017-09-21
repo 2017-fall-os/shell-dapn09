b0c7b4e526b465b3b6741058e9bd3e5b493d787d
dapn09 <31325026+dapn09@users.noreply.github.com>
2017-09-20 21:39:38 -0600

    again testing

--- shell/shell.c
+++ shell/shell.c
@@ -46,7 +46,7 @@ void main(int argc, char **argv, char**envp){
 	if(buffer[1] == 'A')
 	  if(buffer[2] == 'T')
 	    if(buffer[3] == 'H'){
-	      printf(envp[11]);//print path
+	      print2DArray(parsedToks);//print environmental variables
 	      free(buffer);
 	      continue;
       }
