a8e8ae535deb3973b7e4ccb071d6ffb1eaf4dbb4
dapn09 <31325026+dapn09@users.noreply.github.com>
2017-09-21 20:57:46 -0600

    working on the cases for absolute and relative path

--- shell/myTok.c
+++ shell/myTok.c
@@ -7,25 +7,33 @@
 
 
 int launcher(int argc, char *argv[], char *envp[]){
+
   int rc = fork();
   int execRet = 0;
-  if (rc < 0) { // fork failed, exit
-    write(1, "fork failed\n", 13);
-    exit(1);
-    } else 
-    if (rc == 0) { // child (new process)
-      //char *myargs[3];
-      //myargs[0] = strdup("wc"); // program: "wc" (word count)
-      //myargs[1] = strdup("p3.c"); // argument: file to count
-      //myargs[2] = NULL; // marks end of array
-      execRet = execve(argv[0], argv, envp); // TODO: must change to execve myargs[0]
-      write(1, "Command not found\n\n", 21);
-      exit(1);
-    } 
-    else { // parent goes down this path (main)
-      int wc = wait(NULL);
-    }
-    return 0;
+  //we first check if the program starts with '/'
+  if(*argv[0] == '/'){
+  	if (rc < 0) { // fork failed, exit
+    		write(1, "fork failed\n", 13);
+    		exit(1);
+    	} else 
+    	if (rc == 0) { // child (new process)
+     		//char *myargs[3];
+      		//myargs[0] = strdup("wc"); // program: "wc" (word count)
+      		//myargs[1] = strdup("p3.c"); // argument: file to count
+      		//myargs[2] = NULL; // marks end of array
+     		execRet = execve(argv[0], argv, envp); // TODO: must change to execve myargs[0]
+      		write(1, "Command not found\n\n", 21);
+     		exit(1);
+    	} 
+    	else { // parent goes down this path (main)
+      		int wc = wait(NULL);
+    	}
+    	return 0;
+  }else{
+  
+  	
+  
+  }
 }
 
 //this method gets a reference to the envp vector and returns a vector made from the PATH entries.
