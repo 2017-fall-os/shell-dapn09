6bb3ea0bf418701fccbaf0727a78cd9de00e271a
dapn09 <31325026+dapn09@users.noreply.github.com>
2017-09-21 18:33:46 -0600

    Update myTok.c

--- shell/myTok.c
+++ shell/myTok.c
@@ -47,7 +47,7 @@ char ** getPath(char** envp){
 		   if(c[3] == 'H')
 		      if(c[4] == '='){
 			 write(1, "PATH was found!\n", 17);//for debugging purposes only
-			 write(1, c, 30);
+			 write(1, c, 40);
 		     	 pathVec = (char**) myTok(&c[5] , ':');//we pass the remainder of the string and the delimiter set to ':'
 			 return pathVec;
 		      }
