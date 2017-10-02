4b9e2c81975662dd099766392277490f7765999e
dapn09 <31325026+dapn09@users.noreply.github.com>
2017-10-01 20:22:26 -0600

    Update myTok.c

--- shell/myTok.c
+++ shell/myTok.c
@@ -309,12 +309,12 @@ void checkSimPipes(char** parsedToks, char** pathVector, char** envp){
 				dup(pipeFds[0]);
 				close(pipeFds[0]);
 				close(pipeFds[1]);
-				wait(NULL);
+				
 				read(0,buf,sizeof(buf));
 				printf(buf);//for debugging
 				launcher(0, restOfToks, pathVector, envp);//restOfToks
+				wait(NULL);
 				
-				//exit(2);
 			}
 		}else{
 		//write(1, "w10\n", 5);//for debugging
