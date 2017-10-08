76b5f7bef4656b848138e3cb0e9d0fc1744c3d46
student <student@localhost.localdomain>
2017-10-07 18:55:11 -0600

    missing tokenizing by newline

--- shell/shell.c
+++ shell/shell.c
@@ -19,14 +19,16 @@ void main(int argc, char **argv, char**envp){
   //enVars = setEnVar("PATH=\"\"", enVars);
   while(!ex){
 
-    if(!enVarExist("PS1", enVars))
+    if(!enVarExist("PS1", enVars))//if PS1 does not exist, create it.
     enVars = createEnVar("PS1", "$ ", enVars);
-    //set the prompt.
+
     prompt = getEnVar("PS1", enVars);
     if(prompt != 0)//only print prompt if exists.
     fprintf(stderr, "%s", prompt);//write prompt to the screen.
+
+
     char* buffer = (char*)calloc(128, sizeof(char));
-    int ans = read(0, buffer, 128);
+    int ans = read(0, buffer, 128);//read input.
 
     if(ans < 0){//if there is an error in the reading process.
       fprintf(stderr, "%s\n", strerror(errno));
@@ -38,7 +40,7 @@ void main(int argc, char **argv, char**envp){
       exit(1);
     }
 	  
-    if((buffer[0] == 0)||(buffer[0] == '\n')||(buffer[0] == EOF)){
+    if((buffer[0] == 0)||(buffer[0] == '\n')){
 	   //if(buffer[0] == '\n')
            //printf("buffer[0] is: %c\n", buffer[0]); 
 	   continue; //if there is no input, start again.
@@ -52,52 +54,27 @@ void main(int argc, char **argv, char**envp){
     char ** pathVector = getPath(enVars);
     int size1stCom = tokenLen(parsedToks[0]);
     //check for exit key word
-    if(size1stCom > 3)    
-      if(parsedToks[0][0] == 'e')
-	if(parsedToks[0][1] == 'x')
-	  if(parsedToks[0][2] == 'i')
-	    if(parsedToks[0][3] == 't'){
-	      write(1, "Bye!!!\n", 7);
-	      ex = 1;
-	    }
+    if(strcmp(parsedToks[0], "exit") == 0){
+        write(1, "Bye!!!\n", 7);
+        ex = 1;
+    }
 
-      /*if(buffer[0] == 'd')
-	if(buffer[1] == 'l')
-	  if(buffer[2] == 't'){
-	    write(1, "Enter the new delimiter:", 24);
-	    read(0, dlt, 1);
-	    write(1, "New delimiter set\n", 19);
-	    free(buffer);
-	    continue;
-	  }*/
      //check for printing environment vars.    
-     if(size1stCom > 3)    
-      if(parsedToks[0][0] == 'e')//for testing purposes only, remove for final submission
-	if(parsedToks[0][1] == 'n')
-	  if(parsedToks[0][2] == 'v')
-	    if(parsedToks[0][3] == 'p'){
-	      print2DArray(enVars);//print environmental variables
-	      free(buffer);
-	      continue;
-      }
+    if(strcmp(parsedToks[0], "envp") == 0){
+        print2DArray(enVars);//print environmental variables
+        free(buffer);
+        continue;
+    }
 
-      if(size1stCom > 5)    
-       if(parsedToks[0][0] == 'e')//for testing purposes only, remove for final submission
-	if(parsedToks[0][1] == 'x')
-	  if(parsedToks[0][2] == 'p')
-	    if(parsedToks[0][3] == 'o')
-	      if(parsedToks[0][4] == 'r')
-		if(parsedToks[0][5] == 't'){
-		  
-	      enVars = setEnVar(parsedToks[1], enVars);//change variable and update enVars.
-	      free(buffer);
-	      continue;
-		}
+    if(strcmp(parsedToks[0], "export") == 0){
+	enVars = setEnVar(parsedToks[1], enVars);//change variable and update enVars.
+	free(buffer);
+        continue;
+    }
 
      //check for changing directory
-     if(size1stCom == 2)    
-       if(parsedToks[0][0] == 'c')//for testing purposes only, remove for final submission
-	 if(parsedToks[0][1] == 'd'){
+    if(size1stCom == 2)    
+       if(strcmp(parsedToks[0], "cd") == 0){
 	      int res = chdir(parsedToks[1]); //change directory 
 	      if(res != 0){
 	      	fprintf(stderr, "%s\n", strerror(errno));
@@ -106,7 +83,7 @@ void main(int argc, char **argv, char**envp){
 		//fprintf(stderr, "The current dir is: %s\n", getcwd(cwd, sizeof(cwd)));
 	      free(buffer);
 	      continue;
-         }
+       }
 
       char* trimBuff = rmTailSpaces(buffer);//for debugging
       //if the word entered is not exit then pass the string to myTok.
